b8b5f16341b378da60be8adc8b4f3a5a34d5a9c9
Christopher O <fahzee1@gmail.com>
2024-05-22 21:10:46 -0500

    added profile model

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Inventory, InventoryLanguage, InventoryTag, InventoryType
 
-# Register your models here.
+admin.site.register(Inventory)
+admin.site.register(InventoryLanguage)
+admin.site.register(InventoryTag)
+admin.site.register(InventoryType)
+
